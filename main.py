8419b93771c5af4e5414399156a219bc10b622d4
lintosunny <lintosunny111@gmail.com>
2025-08-30 19:50:09 +0530

    data validation component added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import TraningPipelineConfig, DataIngestionConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import TraningPipelineConfig, DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 from networksecurity.exception.exception import NetworkSecurityException
 import sys
@@ -15,3 +16,9 @@ if __name__ == "__main__":
     data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
     dataingestionartifact = data_ingestion.initiate_data_ingestion()
     print(dataingestionartifact)
+
+    data_validation_config = DataValidationConfig(training_pipeline_config=training_pipeline_config)
+    data_validation = DataValidation(data_validation_config=data_validation_config,
+                                     data_ingestion_artifact=dataingestionartifact)
+    data_validation_artifact = data_validation.initiate_data_validation()
+    print(data_validation_artifact)
---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,34 @@
+import yaml
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+import os, sys
+import numpy as np
+import pickle 
+
+def read_yaml_file(file_path: str) -> dict:
+    """
+    Reads a YAML file and returns its contents as a dictionary.
+    """
+    try:
+        with open(file_path, 'rb') as file:
+            content = yaml.safe_load(file)
+        logging.info("YAML file %s read successfully", file_path)
+        return content
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    """
+    Writes a dictionary to a YAML file.
+    """
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'w') as file:
+            yaml.dump(content, file)
+        logging.info("YAML file %s written successfully", file_path)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
